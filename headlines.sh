dc82b2f15c0187eafddfc1c6b7579fc51719a90b
Niraj Menon <norandomtechie@users.noreply.github.com>
2018-06-22 21:03:35 -0400

    Initial commit
    Prints headlines from the BBC

---
+++ headlines.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Second script. This one grabs top headlines from newsapi.org
+# in JSON format, parses them and prints them out in nicely
+# formatted strings.
+# Author: Niraj Menon, 2018
+
+printf "\n"
+tput clear
+
+text=$(curl https://newsapi.org/v2/top-headlines -G -d sources=bbc-news -d apiKey=891aa9896d21492ea6eee28a1355b6c6 -s)
+i="0"
+
+echo $(tput bold && tput setaf 7)"From the BBC:"
+printf "\n"
+
+while [ $i -lt 25 ] && [ "$(echo $text | jq '.articles['"$i"'].title')" != "null" ]
+do
+        title=$(echo $text | jq '.articles['"$i"'].title')
+        echo $(tput setaf 1 && tput bold)${title}
+
+        desc=$(echo "${text}" | jq '.articles['"$i"'].description')
+        echo $(tput setaf 7)${desc}
+        printf "        "
+
+        url=$(echo "${text}" | jq '.articles['"$i"'].url')
+        echo $(tput sgr0 && tput setaf 7)${url}
+        printf "\n"
+
+        i=$[$i+1]
+done
+
+exit
