ed73441d879c993aea75f90c40bb5b35e65a9f46
Niraj Menon <nirajmmenon@gmail.com>
2018-06-27 15:16:25 -0400

    Git test 1

--- headlines.sh
+++ headlines.sh
@@ -10,7 +10,7 @@ tput clear
 text=$(curl https://newsapi.org/v2/top-headlines -G -d sources=bbc-news -d apiKey=891aa9896d21492ea6eee28a1355b6c6 -s)
 i="0"
 
-echo $(tput bold && tput setaf 7)"From the BBC:"
+echo $(tput bold && tput setaf 2)"From the BBC (will change later):"
 printf "\n"
 
 while [ $i -lt 25 ] && [ "$(echo $text | jq '.articles['"$i"'].title')" != "null" ]
